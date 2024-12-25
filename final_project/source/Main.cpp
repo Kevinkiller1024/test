15dd2b90b84783e5f2ecf6a291d1eca360d4462b
Kevinkiller1024 <Kevinkiller9487@gmail.com>
2024-12-25 16:09:27 +0800

    add rull function

--- final_project/source/Main.cpp
+++ final_project/source/Main.cpp
@@ -22,8 +22,6 @@ void clear_screen() {
 	system("cls");
 }
 
-int rull();
-
 struct card {
 	const char* face;
 	const char* suit;
@@ -59,8 +57,94 @@ void shuffle(Card* const wDeck) { // 洗牌
 	
 }
 
-int rull() {
-	return 1;
+int rull(Card card1, Card card2, Card dragonCard, int bet, int big_or_small) {
+	if (card1.value != card2.value && card2.value != (card1.value + 1) && card2.value != card1.value + 2) {
+		if (dragonCard.value > card1.value && dragonCard.value < card2.value) {
+			printf("恭喜！落在範圍內，你贏了！\n");
+			printf("你拿回了 %d $！\n\n", bet);
+			final += bet;
+		}
+		else if (dragonCard.value == card1.value || dragonCard.value == card2.value) {
+			printf("撞柱！你輸了！賠兩倍的錢\n");
+			printf("你賠了 %d $！\n", bet * 2);
+			final -= bet * 2;
+		}
+		else {
+			printf("你輸了！賠錢\n");
+			printf("你賠了 %d $！\n\n", bet);
+			final -= bet;
+		}
+	}
+	else if (card1.value == card2.value) {
+		if (big_or_small == 1) {
+			if (dragonCard.value > card1.value) {
+				printf("恭喜！你押對邊了！\n");
+				printf("你獲得了 %d $！\n\n", bet);
+				final += bet;
+			}
+			else if (dragonCard.value < card1.value) {
+				printf("你輸了！押錯邊摟哈哈\n");
+				printf("你賠了 %d $！\n\n", bet);
+				final -= bet;
+			}
+			else {
+				printf("撞柱！你輸了！賠三倍的錢\n");
+				printf("你賠了 %d $！\n\n", bet * 3);
+				final -= bet * 3;
+			}
+		}
+		else {
+			if (dragonCard.value < card1.value) {
+				printf("恭喜！你押對邊了！\n");
+				printf("你獲得了 %d $！\n\n", bet * 2);
+				final += bet;
+			}
+			else if (dragonCard.value > card1.value) {
+				printf("你輸了！押錯邊摟哈哈\n");
+				printf("你賠了 %d $！\n\n", bet);
+				final -= bet;
+			}
+			else {
+				printf("撞柱！你輸了！賠三倍的錢\n");
+				printf("你賠了 %d $！\n\n", bet * 3);
+				final += bet * 3;
+			}
+		}
+	}
+	else if (card2.value == card1.value + 1) {
+		if (dragonCard.value == card1.value || dragonCard.value == card2.value) {
+			printf("恭喜！撞柱！贏三倍的錢\n");
+			printf("你獲得了 %d $！\n\n", bet * 3);
+			final += bet * 3;
+		}
+		else {
+			printf("可惜沒中！賭性堅強阿\n");
+			printf("你賠了 %d $！\n\n", bet);
+			final -= bet;
+		}
+	}
+	else {
+		if (dragonCard.value > card1.value && dragonCard.value < card2.value) {
+			printf("恭喜！中洞！贏五倍的錢\n");
+			printf("你拿回了 %d $！\n\n", bet * 5);
+			final += bet * 6;
+		}
+		else if (dragonCard.value == card1.value || dragonCard.value == card2.value) {
+			printf("撞柱！你輸了！賠兩倍的錢\n");
+			printf("你賠了 %d $！\n\n", bet * 2);
+			final -= bet * 2;
+		}
+		else {
+			printf("你輸了！賠錢\n");
+			printf("你賠了 %d $！\n\n", bet);
+			final -= bet;
+		}
+	}
+
+	int choise;
+	printf("還要繼續玩嗎？(0：結束 1：繼續)");
+	scanf("%d", &choise);
+	return choise;
 }
 
 int main() {
